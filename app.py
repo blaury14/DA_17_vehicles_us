91a38ca8ac88f9da76d5012375c219e289947d8c
blaury14 <blaury14@gmail.com>
2024-03-10 09:04:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(page_title="Aplicacion Adicional - Bastian Laury")
 # Cargar los datos
 car_data = pd.read_csv('vehicles_us.csv')
 
